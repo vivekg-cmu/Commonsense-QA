0658cf3c85a98a93e45a7b3738bf3e2d7ed0d6fb
pratik <pjayarao@andrew.cmu.edu>
2020-10-07 14:07:36 -0400

    add new methods in utils to save and load

--- BERT_retraining/utils.py
+++ BERT_retraining/utils.py
@@ -2,6 +2,12 @@ import pickle
 
 
 def save_dictionary(dictionary, save_path):
+    """
+    This method is used to save dictionary to a given path in pickle file
+    Args:
+        dictionary (dict): dictionary which has to be saved
+        save_path (str): path where the dictionary has to be saved
+    """
     with open(save_path, 'wb') as handle:
         print("saving model to:", save_path)
         pickle.dump(dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -9,6 +15,15 @@ def save_dictionary(dictionary, save_path):
 
 
 def load_dictionary(load_path):
+    """
+    This method is used to load the dictionary given a path
+    Args:
+        load_path (str): loading dictionary from the given path
+
+    Returns:
+        dictionary (dict): the loaded dictionary
+
+    """
     with open(load_path, 'rb') as handle:
         print("loading data from:", load_path)
         dictionary = pickle.load(handle)
