c1037d17f0a21a01e9b7bfa93bea2647b39960fc
pratik <pjayarao@andrew.cmu.edu>
2020-10-21 13:05:56 -0400

    load input dict from pkl

--- Downstream/Preprocess/Preprocess.py
+++ Downstream/Preprocess/Preprocess.py
@@ -4,7 +4,7 @@ sys.path.append(".")
 import pandas as pd
 from transformers import DistilBertTokenizer
 from tqdm import tqdm
-from Downstream.utils import save_dictionary
+from Downstream.utils import save_dictionary, load_dictionary
 from torch.utils import data
 from Downstream import constants as con
 from Downstream.Preprocess.Dataloaders import DownstreamDataset
@@ -13,18 +13,15 @@ from Downstream.Preprocess.Dataloaders import DownstreamDataset
 class Preprocessor:
 
     def __init__(self):
-        self.train_path = "Downstream/Data/dev_data.csv"
+        self.train_path = "Downstream/Data/train_data.csv"
         self.valid_path = "Downstream/Data/dev_data.csv"
-        self.test_path = "Downstream/Data/dev_data.csv"
 
         self.train_data = None
         self.valid_data = None
-        self.test_data = None
         self.tokenizer = None
 
         self.train_loaders = None
         self.valid_loaders = None
-        self.test_loaders = None
 
         self.input_dict = {
             "train": {
@@ -39,24 +36,16 @@ class Preprocessor:
                 "ans_c": [],
                 "label": []
             },
-            "test": {
-                "ans_a": [],
-                "ans_b": [],
-                "ans_c": [],
-                "label": []
-            }
         }
 
     def setup_tokenizer(self):
         self.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased',
                                                              max_length=256, padding="max_length",
                                                              truncation=True, is_split_into_words=False)
-        print(self.tokenizer)
 
     def load_data(self):
         self.train_data = pd.read_csv(self.train_path)
         self.valid_data = pd.read_csv(self.valid_path)
-        self.test_data = pd.read_csv(self.test_path)
 
     def tokenize_data(self, data, key):
 
@@ -73,26 +62,27 @@ class Preprocessor:
                 self.input_dict[key][ans].append(input_line + [0 for _ in range(512 - len(input_line))])
             self.input_dict[key]["label"].append(labels[i])
 
-    def get_loaders(self):
+    def get_loaders(self, load_from_pkl=False):
+        if load_from_pkl:
+            try:
+                self.input_dict = load_dictionary("Downstream/Data/input_dict.pkl")
+            except Exception as e:
+                print(e)
+
         train_dataset = DownstreamDataset(input_dict=self.input_dict["train"])
-        valid_dataset = train_dataset
-        test_dataset = train_dataset
-
+        valid_dataset = DownstreamDataset(input_dict=self.input_dict["valid"])
         loader_args = dict(shuffle=True, batch_size=con.BATCH_SIZE)
-
         self.train_loaders = data.DataLoader(train_dataset, **loader_args)
         self.valid_loaders = data.DataLoader(valid_dataset, **loader_args)
-        self.test_loaders = data.DataLoader(test_dataset, **loader_args)
 
     def setup(self):
         self.load_data()
         self.setup_tokenizer()
         self.tokenize_data(data=self.train_data, key="train")
         self.tokenize_data(data=self.valid_data, key="valid")
-        self.tokenize_data(data=self.test_data, key="test")
-        # save_dictionary(dictionary=self.input_dict,
-        #                 save_path="Downstream/Data/input_dict.pkl")
-        self.get_loaders()
+        save_dictionary(dictionary=self.input_dict,
+                        save_path="Downstream/Data/input_dict.pkl")
+        self.get_loaders(load_from_pkl=True)
 
 
 if __name__ == '__main__':
