6e3e0e2edaade6d7a1a7cea18da027b22fee9ef6
pratik <pjayarao@andrew.cmu.edu>
2020-10-28 12:10:20 -0400

    add dataloaders with attention

---
+++ CommonSense/Preprocess/Dataloaders.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.path.append(".")
+
+from torch.utils import data
+import numpy as np
+
+
+class DownstreamDataset(data.Dataset):
+    def __init__(self, input_dict):
+        self.input_dict = input_dict
+
+    def __len__(self):
+        return len(self.input_dict["label"])
+
+    def __getitem__(self, index):
+        ans_a = np.array(self.input_dict["ans_a"][index])
+        ans_b = np.array(self.input_dict["ans_b"][index])
+        ans_c = np.array(self.input_dict["ans_c"][index])
+        ans_d = np.array(self.input_dict["ans_d"][index])
+        ans_e = np.array(self.input_dict["ans_e"][index])
+
+        ans_a_att = np.array(self.input_dict["ans_a_att"][index])
+        ans_b_att = np.array(self.input_dict["ans_b_att"][index])
+        ans_c_att = np.array(self.input_dict["ans_c_att"][index])
+        ans_d_att = np.array(self.input_dict["ans_d_att"][index])
+        ans_e_att = np.array(self.input_dict["ans_e_att"][index])
+
+        label = np.array(self.input_dict['label'][index])
+        return ans_a, ans_b, ans_c, ans_d, ans_e, ans_a_att, \
+               ans_b_att, ans_c_att, ans_d_att, ans_e_att, label
