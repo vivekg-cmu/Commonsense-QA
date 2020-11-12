a3d92dd09fccc1a14ebc51af4585b0baf1e6dc51
pratik <pjayarao@andrew.cmu.edu>
2020-11-12 16:57:23 -0500

    add preprocessing skeleton code for tokenization of wikihow

--- BERT_retraining/DataSetup/ParsePiqa.py
+++ BERT_retraining/DataSetup/ParsePiqa.py
@@ -8,23 +8,24 @@ from tqdm import tqdm
 
 class ParsePiqa:
     def __init__(self, source_path="./BERT_retraining/Data/wikihowSep.csv",
-                 dest_path="./BERT_retraining/Data/clean_wikihow.txt"):
+                 dest_path="./BERT_retraining/Data/clean_wikihow_"):
         self.source_path = source_path
-        self.output_writer = open(dest_path, "w")
+        self.output_writer_train = open(dest_path + "train.txt", "w")
+        self.output_writer_valid = open(dest_path + "valid.txt", "w")
         self.data = None
 
     def load_wikihow(self):
         self.data = pd.read_csv(self.source_path)
         self.data.dropna(inplace=True)
 
-    def parse_wikihow(self, text):
+    def parse_wikihow(self, text, output_writer):
         try:
             text = "".join([x.rstrip().strip() for x in text.split("\n") if x])
 
             text_list = [x.strip() + '\n' for x in text.split('.') if len(re.findall("\w+", x)) > 5]
             if text_list:
                 text_list[-1] += '\n'
-                self.output_writer.writelines(text_list)
+                output_writer.writelines(text_list)
         except Exception as e:
             print(e)
             return False
@@ -34,9 +35,18 @@ class ParsePiqa:
     def setup(self):
         self.load_wikihow()
         error_count = 0
+        index = 0
         for text in tqdm(self.data['text']):
-            if not self.parse_wikihow(text):
+
+            if index < int(len(self.data) * 0.95):
+                error_status = self.parse_wikihow(text, self.output_writer_train)
+            else:
+                error_status = self.parse_wikihow(text, self.output_writer_valid)
+
+            if not error_status:
                 error_count += 1
+
+            index += 1
         print(f"Parsing completed with {error_count} errors")
 
 
---
+++ BERT_retraining/DataSetup/PreprocessorWiki.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append(".")
+from BERT_retraining.DataSetup.Pretraining import Pretraining
+from BERT_retraining.DataSetup.Dataloaders import PretrainingDataset
+from BERT_retraining import utils
+from torch.utils import data
+from BERT_retraining import constants as con
+
+class PreprocessorsWiki:
+
+    def __init__(self):
+
+        self.train_loaders = None
+        self.valid_loaders = None
+
+    def run_pretraining(self, path="BERT_retraining/Data/clean_wikihow_", key="train"):
+        pretraining = Pretraining(
+            vocab_file="BERT_retraining/Data/bert-base-uncased-vocab.txt",
+            do_lower_case=True,
+            input_file=path + key + ".txt",
+            random_seed=12345,
+            max_seq_length=32,
+            dupe_factor=4,
+            max_predictions_per_seq=5,
+            masked_lm_prob=0.15,
+            output_file="Data/wikihow_output.txt",
+            short_seq_prob=0.1)
+
+        instances, tokenizer = pretraining.run_data_preprocessing()
+        features = pretraining.write_instance_to_features(instances=instances, tokenizer=tokenizer,
+                                                          max_seq_length=32,
+                                                          max_predictions_per_seq=5)
+        utils.save_dictionary(dictionary=features,
+                              save_path="./BERT_retraining/Data/"
+                                        + key + "_wiki.pkl")
+        return features
+
+    def get_features(self, load_flag=False):
+        if load_flag:
+            train_features = \
+                utils.load_dictionary("BERT_retraining/Data/train_wiki.pkl")
+            valid_features = \
+                utils.load_dictionary("BERT_retraining/Data/valid_wiki.pkl")
+        else:
+            train_features, valid_features = self.run_features()
+
+        return train_features, valid_features
+
+    def run_features(self):
+        train_features = self.run_pretraining(key='train')
+        valid_features = self.run_pretraining(key='train')
+        utils.save_dictionary(dictionary=train_features,
+                              save_path="BERT_retraining/Data/train_wiki.pkl")
+        utils.save_dictionary(dictionary=valid_features,
+                              save_path="BERT_retraining/Data/valid_wiki.pkl")
+        return train_features, valid_features
+
+    def get_loaders(self, load_flag=False):
+        train_features, valid_features = self.get_features(load_flag=load_flag)
+        train_dataset = PretrainingDataset(input_dict=train_features)
+        valid_dataset = PretrainingDataset(input_dict=valid_features)
+
+        loader_args = dict(shuffle=True, batch_size=con.BATCH_SIZE)
+
+        self.train_loaders = data.DataLoader(train_dataset, **loader_args)
+        self.valid_loaders = data.DataLoader(valid_dataset, **loader_args)
+
+if __name__ == '__main__':
+    PreprocessorsWiki().get_loaders(load_flag=False)
