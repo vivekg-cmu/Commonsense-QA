6fece0e17e1a9ff1c8dd030272be2b7e97d2ca2d
pratik <pjayarao@andrew.cmu.edu>
2020-11-12 14:19:54 -0500

    add validation code

--- CommonSense/Trainer/Trainer.py
+++ CommonSense/Trainer/Trainer.py
@@ -4,7 +4,7 @@ sys.path.append(".")
 import torch
 from CommonSense.Preprocess.Preprocessor import Preprocessor
 from CommonSense.Models.DistilBERT import DownstreamModel
-from CommonSenseg import constants as con
+from CommonSense import constants as con
 from torch import optim
 
 
